4d11d75b5ed75197d51981b16cca0ed9b860c318
worrawutK <qsswutssq1@gmail.com>
2017-11-10 14:38:43 +0700

    01

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(68, 72);
+            this.button1.Location = new System.Drawing.Point(53, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 134);
             this.button1.TabIndex = 0;
